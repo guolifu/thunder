01a3d630de2f33f31adfaaa18b7fa488c20a0700
GLF <631929063@qq.com>
2017-09-16 20:49:52 +0800

    add

--- app/Api/controller/indexController.php
+++ app/Api/controller/indexController.php
@@ -1,11 +1,16 @@
 <?php
 namespace api\controller;
-
+header("Access-Control-Allow-Origin: *");
 class IndexController{
     public function index(){
         return "api";
     }
     public function api(){
         return ['api'=>1];
+    }
+    public function get_dish(){
+        $dish = model('dish');
+        $res = $dish->lists();
+        return ($res) ;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use \thunder\Route;
  * 注册路由
  *
  * */
-Route::init('goapi','api/index/index');
+Route::init('goapi','api/index/api');
 
 Route::init('goapi2/add/{param}','api/index/index');
 
 Route::init('gohome/{param}','home/index/url');
+
+Route::init('get_dish','api/index/get_dish');
