7c056370674f22ea0afbe06168d8dbf41882428d
lifu.guo <lifu.guo@ironfans.cn>
2018-04-09 11:43:45 +0800

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $_root  =   rtrim(dirname(_PHP_FILE_),'/');
 define('__ROOT__',  (($_root=='/' || $_root=='\\')?'':$_root));
 define('__PUBLIC__',  __ROOT__.'/public');
 define('__UPLOAD__','./public/uploads');
-define('THUNDER',realpath('./'));
+define('THUNDER',__DIR__);
 define('CORE',THUNDER.'/core');
 define('APP',THUNDER.'/app');
 define('ROUTES',THUNDER.'/routes');
